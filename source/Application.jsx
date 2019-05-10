56a7bf9ce386e903bf6e14421d5bbb04cc7f372f
Ilya Denisov <i.denisovs@gmail.com>
2019-05-10 13:06:33 +0300

    Initial commit for calendar project.

---
+++ source/Application.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import style from './Application.scss';
+
+class Application extends React.Component {
+    render() {
+        return (
+            <div className={style['Application']}>
+
+            </div>
+        )
+    }
+}
+
+export default Application
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const {join, resolve} = require('path');
+
+const sourcesPath = resolve(__dirname, './source');
+console.log('Sources:', sourcesPath);
+
+const entryFile = join(sourcesPath, 'index.jsx');
+console.log('Entry file:', entryFile);
+
+const target = resolve('./target');
+console.log('Destination:', target);
+
+const react = {
+    test: /\.js[x]?$/,
+    exclude: /node_modules/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/env', '@babel/react']
+        }
+    }
+};
+
+const style = {
+    test: /\.[s]?css$/,
+    exclude: /node_modules/,
+    use: [
+        { loader: 'style-loader' },
+        {
+            loader: 'css-loader',
+            options: {
+                modules: true
+            }
+        },
+        { loader: 'sass-loader' }
+    ]
+};
+
+module.exports = (env, argv) => ({
+    mode: "development",
+    entry: entryFile,
+    devtool: getDevTool(argv),
+    devServer: {
+        contentBase: target
+    },
+    resolve: {
+        extensions: [ '.js', '.jsx' ]
+    },
+    module: {
+        rules: [
+            react, style
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: target
+    }
+});
+
+function getDevTool(argv) {
+    if (argv.mode === 'production') {
+        return '';
+    }
+
+    return 'source-map';
+}
