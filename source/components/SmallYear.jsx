7ab6774932925397c928cd2587f2d1ac6fa7809b
I. Denisovs <i.denisovs@gmail.com>
2019-10-27 12:21:45 +0200

    Possibility to select year is reduced to +/- 1 year.

--- source/components/SmallYear.jsx
+++ source/components/SmallYear.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import mod from '../services/mod';
 
 import style from './SmallYear.scss';
 
 class SmallYear extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.isClickable = true;
+    }
+
     render() {
+        this.isClickable = !this.isYearBoundaryReached();
+
+        const clickable = { [style['clickable']]: this.isClickable };
+        const styles = classNames(style.SmallYear, clickable);
+
         return (
-            <h2 className={style.SmallYear}>
+            <h2 className={styles}>
                 {this.renderLabel()}
             </h2>
         );
@@ -15,27 +27,34 @@ class SmallYear extends React.Component {
 
     renderLabel() {
         const {year} = this.props;
-        const currentYear = (new Date()).getFullYear();
-        const nextYear = currentYear + 1;
-        const prevYear = currentYear - 1;
-
-        if (prevYear > year || year > nextYear) {
-            return null;
-        }
+        const label = `0x${mod(year).toUpperCase()}`;
 
         return (
             <span onClick={this.handleClick.bind(this)}>
-                0x{mod(year).toUpperCase()}
+                {label}
             </span>
         )
     }
     
     handleClick() {
-        const {year, onClick} = this.props;
-        
-        if (onClick) {
-            onClick(year);
+        const {onClick} = this.props;
+
+        if (!this.isClickable || !onClick) {
+            return;
         }
+
+        onClick(this.props.year);
+    }
+
+    isYearBoundaryReached() {
+        const {year} = this.props;
+
+        const currentYear = (new Date()).getFullYear();
+
+        const nextYear = currentYear + 1;
+        const prevYear = currentYear - 1;
+
+        return prevYear > year || year > nextYear;
     }
 }
 
--- source/components/Year.jsx
+++ source/components/Year.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import SmallYear from './SmallYear';
 import mod from '../services/mod';
 
 import style from './Year.scss';
-import SmallYear from "./SmallYear";
 
 class Year extends React.Component {
     constructor(props) {
